f8de959a5c5b4a2635b903d5f561611d5d824113
Griffin Adams <griff4692@gmail.com>
2019-10-02 12:50:14 -0400

    Multi C.

--- main.py
+++ main.py
@@ -5,78 +5,136 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 
-C = 2
+RANDOM_SEED = 1928
+np.random.seed(RANDOM_SEED)
 
 
-def sample(positive_prob):
-    return 1 if np.random.random() <= positive_prob else 0
+def accuracy(z, y):
+    party_map = {'D': 0, 'R': 1}
+    y_r = np.array(list(map(lambda a: party_map[a], y)))
+    acc = len(np.where(z == y_r)[0]) / float(len(z))
+    if acc < 0.5:  # Give the benefit of the doubt about assignments aligning with proper values
+        acc = 1.0 - acc
+    return acc
 
 
-def assignment_accuracy(z, y):
-    possible_y = [1 if a == 'D' else 0 for a in y]
-    acc = _accuracy(z, possible_y)
-    possible_y = [0 if a == 'D' else 1 for a in y]
-    return max(_accuracy(z, possible_y), acc)
+def calculate_log_joint(args, X, z, components):
+    log_joint = 0.0
+    for cidx in range(args.C):
+        for component_bernoulli in components[cidx]:
+            log_joint += np.log(
+                scipy.stats.beta(args.component_alpha, args.component_beta).pdf(component_bernoulli))
+    for n in range(X.shape[0]):
+        x = X[n, :]
+        assignment = z[n]
+        assignment_components = components[assignment]
+        log_joint += np.log(thetas[assignment])
+        for vidx in range(x_dim):
+            vote = x[vidx]
+            vote_pi = assignment_components[vidx]
+            if vote > -1:
+                log_joint += np.log(1.0 - vote_pi) if vote == 0 else np.log(vote_pi)
+    return log_joint
 
 
-def _accuracy(z, y):
-    acc = 0.0
-    for a, b in zip(z, y):
-        if a == b:
-            acc += 1
-    return acc / float(len(z))
+def changed_assignments(a, b):
+    return len(np.where(a != b)[0])
+
+
+def resample_assignments(args, X, components, z, argmax=False):
+    for n_idx in range(X.shape[0]):
+        log_likelihoods = [0.0] * args.C
+        for c_idx in range(args.C):
+            log_likelihoods[c_idx] += np.log(thetas[c_idx])
+            for vote_outcome, vote_pi in zip(X[n_idx], components[c_idx]):
+                if vote_outcome > -1:
+                    prob = 1 - vote_pi if vote_outcome == 0 else vote_pi
+                    log_likelihoods[c_idx] += np.log(prob)
+        likelihoods = [np.exp(l) for l in log_likelihoods]
+        lsum = float(sum(likelihoods))
+        likelihoods_normalized = [l / lsum for l in likelihoods]
+        if argmax:
+            z_assignment = np.argmax(np.array(likelihoods_normalized))
+        else:
+            z_assignment = np.random.choice(args.C, 1, p=likelihoods_normalized)[0]
+        z[n_idx] = z_assignment
+
+
+def run_validation_tests(args, dev_X, dev_y):
+    dev_z = np.random.random_integers(0, high=args.C - 1, size=(dev_X.shape[0], ))
+    resample_assignments(args, dev_X, components, dev_z)
+    dev_log_joint = calculate_log_joint(args, dev_X, dev_z, components) / float(dev_X.shape[0])
+    print('Validation Log Joint: {}'.format(dev_log_joint))
+    print('Validation Accuracy: {}'.format(accuracy(dev_z, dev_y)))
+    dev_republicans = len([d for d in dev_y if d == 'R'])
+    dev_democrats = len([d for d in dev_y if d == 'D'])
+    print('Validation Splits: Democrats={}, Republicans={}'.format(dev_democrats, dev_republicans))
+
+
+def train_dev_splits(x, y, train_fract=0.8):
+    data_idxs = np.random.permutation(np.arange(len(x)))
+    train_split_idx = round(len(x) * train_fract)
+
+    train_x, train_y, dev_x, dev_y = [], [], [], []
+    for count, data_idx in enumerate(data_idxs):
+        if count < train_split_idx:
+            train_x.append(x[data_idx])
+            train_y.append(y[data_idx])
+        else:
+            dev_x.append(x[data_idx])
+            dev_y.append(y[data_idx])
+
+    return (np.array(train_x), train_y), (np.array(dev_x), dev_y)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', default='~/Desktop')
-
+    parser.add_argument('--C', type=float, default=2)
     parser.add_argument('--component_alpha', type=float, default=1.0)
     parser.add_argument('--component_beta', type=float, default=1.0)
-
-    parser.add_argument('--proportion_alpha', type=float, default=10.0)
-    parser.add_argument('--proportion_beta', type=float, default=10.0)
+    parser.add_argument('--data_dir', default='~/Desktop')
+    parser.add_argument('--proportion_alpha', type=float, default=1.0)
+    parser.add_argument('-use_validation', default=True, action='store_true')
 
     args = parser.parse_args()
     args.data_dir = os.path.expanduser(args.data_dir)
 
+    # Load data and political party affiliations (treat as unobserved latent assignment)
     votes_df = pd.read_csv(os.path.join(args.data_dir, 'senate', 'votes.csv'))
     X = votes_df.to_numpy()
-    N = X.shape[0]
     x_dim = X.shape[1]
     y = list(map(
         lambda a: a.split('(')[1].split('-')[0],
         open(os.path.join(args.data_dir, 'senate', 'senators.txt')).readlines()))
 
-    proportion_beta_prior = (args.proportion_alpha, args.proportion_beta)
-    theta = 0.5  # assume an equal proportion of Dems and Republicans
+    dev_X, dev_y = None, None
+    if args.use_validation:
+        (X, y), (dev_X, dev_y) = train_dev_splits(X, y)
+    N = X.shape[0]
 
+    # Assume uniform proportions prior
+    thetas = np.ones([args.C,]) / float(args.C)
+
+    # Each party has its own probability of voting for each bill.  These are Bernoullis with Beta(1, 1) priors
     components = []
-    for _ in range(C):
+    for _ in range(args.C):
         components.append(np.random.beta(args.component_alpha, args.component_beta, size=(x_dim,)))
 
-    z = np.random.random_integers(0, high=1, size=(N, ))
+    z = np.random.random_integers(0, high=args.C - 1, size=(N, ))  # Gets over-written so doesn't really matter
 
-    MAX_ITERS = 10
+    MAX_ITERS = 100
     for iter in range(MAX_ITERS):
-        for n_idx in range(N):
-            log_likelihoods = [0.0, 0.0]
-            for c_idx in range(C):
-                prior_assignment = theta if c_idx == 1 else 1.0 - theta
-                log_likelihoods[c_idx] += np.log(prior_assignment)
-                for vote_outcome, vote_pi in zip(X[n_idx], components[c_idx]):
-                    if vote_outcome > -1:
-                        prob = 1 - vote_pi if vote_outcome == 0 else vote_pi
-                        log_likelihoods[c_idx] += np.log(prob)
-            z_prob = np.exp(log_likelihoods[1]) / (np.exp(log_likelihoods[0]) + np.exp(log_likelihoods[1]))
-            z_assignment = sample(z_prob)
-            z[n_idx] = z_assignment
+        prev_z = z.copy()
+        resample_assignments(args, X, components, z)
 
-        pos_assignments = sum(z)
-        neg_assignments = N - pos_assignments
-        theta = np.random.beta(args.proportion_alpha + pos_assignments, args.proportion_beta + neg_assignments)
+        # Resample assignment proportions from dirichlet prior and current assignment proportions
+        dirichlet_alphas = [args.proportion_alpha] * args.C
+        for z_assignment in z:
+            dirichlet_alphas[z_assignment] += 1
+        thetas = np.random.dirichlet(dirichlet_alphas)
 
-        for cidx in range(C):
+        # Update per-vote Bernoulli means separately for each cluster based on current cluster assignments
+        for cidx in range(args.C):
             component_x = X[np.where(z == cidx)[0]]
             for vote_idx in range(x_dim):
                 component_votes = list(filter(lambda x: x >= 0, component_x[:, vote_idx]))
@@ -85,24 +143,7 @@ if __name__ == '__main__':
                 component_sample = np.random.beta(args.component_alpha + pos_votes, args.component_beta + neg_votes)
                 components[cidx][vote_idx] = component_sample
 
-        log_joint = 0.0
-        log_joint += np.log(scipy.stats.beta(args.proportion_alpha, args.proportion_beta).pdf(theta))
-
-        for cidx in range(C):
-            for component_bernoulli in components[cidx]:
-                log_joint += np.log(
-                    scipy.stats.beta(args.component_alpha, args.component_beta).pdf(component_bernoulli))
-        for n in range(N):
-            x = X[n, :]
-            assignment = z[n]
-            assignment_components = components[assignment]
-            log_joint += np.log(theta if assignment == 1 else 1.0 - theta)
-
-            for vidx in range(x_dim):
-                vote = x[vidx]
-                vote_pi = assignment_components[vidx]
-                if vote > -1:
-                    log_joint += np.log(1.0 - vote_pi) if vote == 0 else np.log(vote_pi)
-
-        print(log_joint)
-        print(assignment_accuracy(z, y))
+        log_joint = calculate_log_joint(args, X, z, components)
+        print('Number of changed assignments: {}'.format(changed_assignments(prev_z, z)))
+        print('Normalized Log Joint: {}'.format(log_joint / float(X.shape[0])))
+        args.use_validation and run_validation_tests(args, dev_X, dev_y)
