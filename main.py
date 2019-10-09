cdd5b7b41a3f708f5e9b7c5538c3db1bcf20fc01
Griffin Adams <griff4692@gmail.com>
2019-10-08 20:34:53 -0400

    Add burnin and lag.

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def accuracy(z, y):
     return acc
 
 
-def calculate_log_joint(args, X, z, components):
+def calculate_log_joint(args, X, z, components, thetas):
     log_joint = 0.0
     for cidx in range(args.C):
         for component_bernoulli in components[cidx]:
@@ -33,8 +33,34 @@ def calculate_log_joint(args, X, z, components):
             vote = x[vidx]
             vote_pi = assignment_components[vidx]
             if vote > -1:
-                log_joint += np.log(1.0 - vote_pi) if vote == 0 else np.log(vote_pi)
+                vote_ll = np.log(1.0 - vote_pi) if vote == 0 else np.log(vote_pi)
+                log_joint += vote_ll
     return log_joint
+
+
+def calculate_log_posterior_predictive(X, components, thetas):
+    """
+    :param X: Vote Data
+    :param components: Posterior sample of Beta^hat
+    :param thetas: Posterior sample of theta^hat
+    :return: Log Posterior Predictive
+    """
+    log_pp = 0.0
+    for n in range(X.shape[0]):
+        assignment_lprobs = [0.0] * len(components)
+        x = X[n, :]
+        for assignment in range(len(components)):
+            assignment_components = components[assignment]
+            assignment_lprobs[assignment] += np.log(thetas[assignment])
+            for vidx in range(x_dim):
+                vote = x[vidx]
+                vote_pi = assignment_components[vidx]
+                if vote > -1:
+                    vote_ll = np.log(1.0 - vote_pi) if vote == 0 else np.log(vote_pi)
+                    assignment_lprobs[assignment] += vote_ll
+        assignment_probs = [np.exp(p) for p in assignment_lprobs]
+        log_pp += np.log(sum(assignment_probs))
+    return log_pp
 
 
 def changed_assignments(a, b):
@@ -60,11 +86,13 @@ def resample_assignments(args, X, components, z, argmax=False):
         z[n_idx] = z_assignment
 
 
-def run_validation_tests(args, dev_X, dev_y):
+def run_validation_tests(args, dev_X, dev_y, components, thetas):
     dev_z = np.random.random_integers(0, high=args.C - 1, size=(dev_X.shape[0], ))
     resample_assignments(args, dev_X, components, dev_z, argmax=True)
-    dev_log_joint = calculate_log_joint(args, dev_X, dev_z, components) / float(dev_X.shape[0])
+    dev_log_joint = calculate_log_joint(args, dev_X, dev_z, components, thetas) / float(dev_X.shape[0])
+    dev_log_posterior_predictive = calculate_log_posterior_predictive(dev_X, components, thetas) / float(dev_X.shape[0])
     print('Validation Log Joint: {}'.format(dev_log_joint))
+    print('Approximation to Posterior Predictive: {}'.format(dev_log_posterior_predictive))
     print('Validation Accuracy: {}'.format(accuracy(dev_z, dev_y)))
     dev_republicans = len([d for d in dev_y if d == 'R'])
     dev_democrats = len([d for d in dev_y if d == 'D'])
@@ -95,6 +123,9 @@ if __name__ == '__main__':
     parser.add_argument('--data_dir', default='~/Desktop')
     parser.add_argument('--proportion_alpha', type=float, default=1.0)
     parser.add_argument('-use_validation', default=True, action='store_true')
+    parser.add_argument('--burnin', type=int, default=100)
+    parser.add_argument('--lag', type=int, default=100)
+    parser.add_argument('--num_samples', type=int, default=10)
 
     args = parser.parse_args()
     args.data_dir = os.path.expanduser(args.data_dir)
@@ -114,16 +145,18 @@ if __name__ == '__main__':
 
     # Assume uniform proportions prior
     thetas = np.ones([args.C,]) / float(args.C)
+    theta_samples = []
 
     # Each party has its own probability of voting for each bill.  These are Bernoullis with Beta(1, 1) priors
-    components = []
+    components, component_samples = [], []
     for _ in range(args.C):
-        components.append(np.random.beta(args.component_alpha, args.component_beta, size=(x_dim,)))
+        component_init = np.random.beta(args.component_alpha, args.component_beta, size=(x_dim,))
+        components.append(component_init)
+        component_samples.append([])
 
     z = np.random.random_integers(0, high=args.C - 1, size=(N, ))  # Gets over-written so doesn't really matter
 
-    MAX_ITERS = 100
-    for iter in range(MAX_ITERS):
+    for iter in range(args.burnin + (args.num_samples - 1) * args.lag):
         prev_z = z.copy()
         resample_assignments(args, X, components, z)
 
@@ -143,7 +176,16 @@ if __name__ == '__main__':
                 component_sample = np.random.beta(args.component_alpha + pos_votes, args.component_beta + neg_votes)
                 components[cidx][vote_idx] = component_sample
 
-        log_joint = calculate_log_joint(args, X, z, components)
+        log_joint = calculate_log_joint(args, X, z, components, thetas)
         print('Number of changed assignments: {}'.format(changed_assignments(prev_z, z)))
         print('Normalized Log Joint: {}'.format(log_joint / float(X.shape[0])))
-        args.use_validation and run_validation_tests(args, dev_X, dev_y)
+
+        if (iter + 1) >= args.burnin and (iter + 1) % args.lag == 0:
+            print('Collecting sample after {} iterations'.format(iter + 1))
+            theta_samples.append(thetas)
+            for cidx in range(args.C):
+                component_samples[cidx].append(components[cidx])
+
+    sample_component_means = [cs.mean(0) for cs in np.array(component_samples)]
+    sample_theta_means = np.array(theta_samples).mean(0)
+    args.use_validation and run_validation_tests(args, dev_X, dev_y, sample_component_means, sample_theta_means)
